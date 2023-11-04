9e33dab2a1392fbbcd853fc8b4e976778ded8a8d
wachid <wachid@rakhasa.com>
2023-11-04 15:55:28 +0700

    fix(home): remove unnecesarry async keyword

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { redirect } from 'next/navigation';
 import { getInfo } from '~/lib/api';
 const i = getInfo();
 
-export default async function Page() {
+const Page = () => {
   redirect('/' + i.app.slug);
-}
+};
+
+export default Page;
